1bafde2b150ff6fb9224f16be0891b00b25ac314
Tatev Gomtsyan <90347726+tatevmane@users.noreply.github.com>
2023-08-10 21:20:30 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,2 @@
+import setuptoolsfrom setuptools 
+import setup, find_packagessetup(    name = 'montecarlo',    version = '0.1.0',    author = 'Tatev Gomtsyan',     url = 'git@github.com:tatevmane/booklover.git',    author_email = 'tmg6jda@virginia.edu',    license = 'MIT')
